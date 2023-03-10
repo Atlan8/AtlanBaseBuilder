e27e5b2b765fcc764ebe6451afed2fc9568a99a3
Atlan <756777965@qq.com>
2023-03-10 23:50:28 +0800

    feat:调整装机详情组件hook

--- src/views/assemble/components/assembleInfo.vue
+++ src/views/assemble/components/assembleInfo.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <div v-if="formData">
-      <el-form :model="info" label-width="150px" label-position="right">
+      <el-form :model="props.info" label-width="150px" label-position="right">
         <el-form-item label="方案名称">
           <el-input
             v-model="formData.name"
             style="width: 250px"
-            v-show="isForm"
+            v-show="props.isForm"
           ></el-input>
-          <div v-show="!isForm">{{ info.name }}</div>
+          <div v-show="!props.isForm">{{ props.info.name }}</div>
         </el-form-item>
         <el-form-item label="CPU">
           <div>
             <AssembleSpecification
               v-model:item="formData.cpu"
-              :is-form="isForm"
+              :is-form="props.isForm"
             />
           </div>
         </el-form-item>
@@ -22,7 +22,7 @@
           <div>
             <AssembleSpecification
               v-model:item="formData.motherboard"
-              :is-form="isForm"
+              :is-form="props.isForm"
             />
           </div>
         </el-form-item>
@@ -30,7 +30,7 @@
           <div>
             <AssembleSpecification
               v-model:item="formData.memory"
-              :is-form="isForm"
+              :is-form="props.isForm"
             />
           </div>
         </el-form-item>
@@ -38,7 +38,7 @@
           <div>
             <AssembleSpecification
               v-model:item="formData.radiator"
-              :is-form="isForm"
+              :is-form="props.isForm"
             />
           </div>
         </el-form-item>
@@ -50,7 +50,7 @@
             >
               <AssembleSpecification
                 v-model:item="formData.hardDiskList[index]"
-                :is-form="isForm"
+                :is-form="props.isForm"
               />
             </div>
           </div>
@@ -59,7 +59,7 @@
           <div>
             <AssembleSpecification
               v-model:item="formData.graphicsCard"
-              :is-form="isForm"
+              :is-form="props.isForm"
             />
           </div>
         </el-form-item>
@@ -67,7 +67,7 @@
           <div>
             <AssembleSpecification
               v-model:item="formData.powerSupply"
-              :is-form="isForm"
+              :is-form="props.isForm"
             />
           </div>
         </el-form-item>
@@ -75,7 +75,7 @@
           <div>
             <AssembleSpecification
               v-model:item="formData.chassis"
-              :is-form="isForm"
+              :is-form="props.isForm"
             />
           </div>
         </el-form-item>
@@ -83,17 +83,17 @@
           <div>
             <AssembleSpecification
               v-model:item="formData.fan"
-              :is-form="isForm"
+              :is-form="props.isForm"
             />
           </div>
         </el-form-item>
         <el-form-item label="总价">
-          <div v-show="!isForm">￥{{ info.total }}</div>
-          <div v-show="!!isForm">￥{{ totalPrice }}</div>
+          <div v-show="!props.isForm">￥{{ props.info.total }}</div>
+          <div v-show="!!props.isForm">￥{{ totalPrice }}</div>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="handleConfirm">确定</el-button>
-          <el-button>取消</el-button>
+          <el-button @click="handleGoBack">取消</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -101,112 +101,16 @@
 </template>
 
 <script lang="ts" setup>
-import { type PropType, onMounted, ref, computed } from "vue";
-import type { AssembleInfo } from "../service";
+import { useAssembleInfo } from "../hooks/useAssembleInfo";
 import AssembleSpecification from "./assembleSpecification.vue";
-import data from "../data.json";
 
-const props = defineProps({
-  info: {
-    type: Object as PropType<AssembleInfo>,
-    required: true,
-  },
-  isForm: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-let formData = ref<AssembleInfo>();
-
-// const totalPrice = ref(0);
-
-/**
- * computed 监听 ref 对象，使用 reactive 的不生效
- */
-const totalPrice = computed(() => {
-  if (!formData.value) return 0;
-  const hard = formData.value.hardDiskList.reduce((prev, cur) => {
-    if (!cur.count) return prev;
-    return prev + cur.count * cur.price;
-  }, 0);
-
-  const cpuPrice = +formData.value.cpu.price;
-  const motherboardPrice = +formData.value.motherboard.price;
-  const memoryPrice =
-    +(formData.value.memory?.count ?? 0) * +formData.value.memory.price;
-  const radiatorPrice = +formData.value.radiator.price;
-  const graphicsCardPrice = +formData.value.graphicsCard.price;
-  const powerSupplyPrice = +formData.value.powerSupply.price;
-  const fanPrice =
-    +(formData.value.fan?.count ?? 0) * +formData.value.fan.price;
-  const chassisPrice = +formData.value.chassis.price;
-
-  console.log("表单数据变化", formData);
-
-  const total =
-    hard +
-    cpuPrice +
-    motherboardPrice +
-    memoryPrice +
-    radiatorPrice +
-    graphicsCardPrice +
-    powerSupplyPrice +
-    chassisPrice +
-    fanPrice;
-  // debugger;
-  return total;
-});
-
-onMounted(() => {
-  formData.value = props.info;
-  console.log(JSON.stringify(props.info));
-  // totalPrice.value = props.info.total;
-});
-
-// watch(props.info, (newVal: AssembleInfo) => {
-//   formData = { ...newVal };
-// });
-
-/**
- * 监听数据变化，动态调整总价 监听 ref 对象，使用 reactive 的不生效
- */
-// watch(
-//   formData,
-//   (newVal) => {
-//     console.log("表单数据变化", { ...newVal });
-//     if (!newVal) return;
-//     const hard = newVal.hardDiskList.reduce((prev, cur) => {
-//       if (!cur.total) return prev;
-//       return prev + cur.total;
-//     }, 0);
-
-//     const total =
-//       +newVal.cpu.price +
-//       +newVal.motherboard.price +
-//       +(newVal.memory?.total ?? 0) +
-//       +newVal.radiator.price +
-//       hard +
-//       +newVal.graphicsCard.price +
-//       +newVal.powerSupply.price +
-//       +newVal.chassis.price +
-//       +(newVal.fan?.total ?? 0);
-//     // debugger;
-//     totalPrice.value = total;
-//   },
-//   { deep: true, immediate: true, flush: "sync" }
-// );
-
-// const handleCPUChange = (val: AccessoriesInfoExt, type: string) => {
-//   formData.value[type] = val;
-//   console.log(val, type, formData.value.cpu);
-// };
-
-const handleConfirm = () => {
-  console.log("---> 修改后的数据", JSON.stringify(formData.value));
-  data.push(JSON.parse(JSON.stringify(formData.value)));
-  // console.log(process.env);
-};
+const {
+  props,
+  formData,
+  totalPrice,
+  handleConfirm,
+  handleGoBack
+} = useAssembleInfo()
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/views/assemble/hooks/useAssembleInfo.ts
@@ -0,0 +1,126 @@
+import { type PropType, onMounted, ref, computed, defineProps } from "vue";
+import { useRouter } from "vue-router";
+import type { AssembleInfo } from "../service";
+
+/**
+ * 装机详情的hook
+ * @returns 
+ */
+export const useAssembleInfo = () => {
+
+  const router = useRouter();
+  
+
+  const props = defineProps({
+    info: {
+      type: Object as PropType<AssembleInfo>,
+      required: true,
+    },
+    isForm: {
+      type: Boolean,
+      default: false,
+    },
+  });
+
+  let formData = ref<AssembleInfo>();
+
+  // const totalPrice = ref(0);
+
+  /**
+   * computed 监听 ref 对象，使用 reactive 的不生效
+   */
+  const totalPrice = computed(() => {
+    if (!formData.value) return 0;
+    const hard = formData.value.hardDiskList.reduce((prev, cur) => {
+      if (!cur.count) return prev;
+      return prev + cur.count * cur.price;
+    }, 0);
+
+    const cpuPrice = +formData.value.cpu.price;
+    const motherboardPrice = +formData.value.motherboard.price;
+    const memoryPrice =
+      +(formData.value.memory?.count ?? 0) * +formData.value.memory.price;
+    const radiatorPrice = +formData.value.radiator.price;
+    const graphicsCardPrice = +formData.value.graphicsCard.price;
+    const powerSupplyPrice = +formData.value.powerSupply.price;
+    const fanPrice =
+      +(formData.value.fan?.count ?? 0) * +formData.value.fan.price;
+    const chassisPrice = +formData.value.chassis.price;
+
+    console.log("表单数据变化", formData);
+
+    const total =
+      hard +
+      cpuPrice +
+      motherboardPrice +
+      memoryPrice +
+      radiatorPrice +
+      graphicsCardPrice +
+      powerSupplyPrice +
+      chassisPrice +
+      fanPrice;
+    // debugger;
+    return total;
+  });
+
+  onMounted(() => {
+    formData.value = props.info;
+    console.log(JSON.stringify(props.info));
+    // totalPrice.value = props.info.total;
+  });
+
+  // watch(props.info, (newVal: AssembleInfo) => {
+  //   formData = { ...newVal };
+  // });
+
+  /**
+   * 监听数据变化，动态调整总价 监听 ref 对象，使用 reactive 的不生效
+   */
+  // watch(
+  //   formData,
+  //   (newVal) => {
+  //     console.log("表单数据变化", { ...newVal });
+  //     if (!newVal) return;
+  //     const hard = newVal.hardDiskList.reduce((prev, cur) => {
+  //       if (!cur.total) return prev;
+  //       return prev + cur.total;
+  //     }, 0);
+
+  //     const total =
+  //       +newVal.cpu.price +
+  //       +newVal.motherboard.price +
+  //       +(newVal.memory?.total ?? 0) +
+  //       +newVal.radiator.price +
+  //       hard +
+  //       +newVal.graphicsCard.price +
+  //       +newVal.powerSupply.price +
+  //       +newVal.chassis.price +
+  //       +(newVal.fan?.total ?? 0);
+  //     // debugger;
+  //     totalPrice.value = total;
+  //   },
+  //   { deep: true, immediate: true, flush: "sync" }
+  // );
+
+  // const handleCPUChange = (val: AccessoriesInfoExt, type: string) => {
+  //   formData.value[type] = val;
+  //   console.log(val, type, formData.value.cpu);
+  // };
+
+  const handleConfirm = () => {
+    console.log("---> 修改后的数据", JSON.stringify(formData.value));
+    // console.log(process.env);
+  };
+
+  const handleGoBack = () => {
+    router.go(-1)
+  }
+
+  return {
+    props,
+    formData,
+    totalPrice,
+    handleConfirm,
+    handleGoBack
+  }
+}
